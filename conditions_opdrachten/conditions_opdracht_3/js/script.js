7375ee525b689b2cf6887d2c94c877a41a99e8db
Daan de jager <daandejager2002@gmail.com>
2019-11-25 15:13:24 +0100

    3/17

--- conditions_opdrachten/conditions_opdracht_3/js/script.js
+++ conditions_opdrachten/conditions_opdracht_3/js/script.js
@@ -1,1 +1,13 @@
 //javascript code
+function check() {
+    var saldo = document.getElementById("saldo").value;
+
+    if(saldo==500) {
+        document.getElementById("tekst").innerHTML = "Uw salaris is gestort";
+    } else if(saldo>=500) {
+        document.getElementById("tekst").innerHTML = "Uw heeft een bonus gekregen   "
+    }
+    else{
+        document.getElementById1("tekst").innerHTML = "Uw salaris is niet gestort"
+    }
+}
