3de1115d398d670e447853bda9455818864729fd
Daan de jager <daandejager2002@gmail.com>
2019-11-25 14:51:29 +0100

    1/17

--- conditions_opdrachten/conditions_opdracht_1/js/script.js
+++ conditions_opdrachten/conditions_opdracht_1/js/script.js
@@ -1,1 +1,10 @@
 //javascript code
+function check() {
+    var saldo = document.getElementById("saldo").value;
+
+    if(saldo<25) {
+        document.getElementById("tekst").innerHTML = "Sorry, je hebt te weinig saldo!";
+    } else {
+        document.getElementById("tekst").innerHTML = "Gefeliciteerd! U heeft meer geld dan Daan de Jager"
+    }
+}
