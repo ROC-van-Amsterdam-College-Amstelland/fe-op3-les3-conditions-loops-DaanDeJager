65fbbd62ad464aabce422868ac5f85dbb486eb16
Daan de jager <daandejager2002@gmail.com>
2019-11-25 15:32:19 +0100

    4/17

--- conditions_opdrachten/conditions_opdracht_4/js/script.js
+++ conditions_opdrachten/conditions_opdracht_4/js/script.js
@@ -1,1 +1,8 @@
 //javascript code
+function check() {
+    var myNumber = document.getElementById("saldo").value;
+
+    if(myNumber == 10) {
+        document.getElementById("tekst").innerHTML = "De variabele heeft de waarde: " + myNumber;
+    }
+}
