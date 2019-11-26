0f5e9b0548960530d64e9d6ad1de8ec7b62b8e8b
Daan de jager <daandejager2002@gmail.com>
2019-11-26 13:41:13 +0100

    8/17

--- loops_opdrachten/loops_opdracht_1/js/script.js
+++ loops_opdrachten/loops_opdracht_1/js/script.js
@@ -1,1 +1,8 @@
 //javascript code
+var getal = ["1", "2", "3", "4", "5"];
+var text = "";
+var i;
+for (i = 0; i < getal.length; i++) {
+  text += getal[i] + "<br>";
+}
+document.getElementById("getal").innerHTML = text;
