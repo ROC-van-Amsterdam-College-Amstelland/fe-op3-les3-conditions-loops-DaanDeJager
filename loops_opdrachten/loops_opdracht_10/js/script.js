fc027cb5678b4217bc087ecf0e09cb4d3d126ff8
Daan de jager <daandejager2002@gmail.com>
2019-12-03 13:52:41 +0100

    ik weet niet waarom dit er nog staat

--- loops_opdrachten/loops_opdracht_10/js/script.js
+++ loops_opdrachten/loops_opdracht_10/js/script.js
@@ -1,1 +1,12 @@
 //javascript code
+var letter = ["A", "B", "C", "D","E"]
+for (let i = 0; i <= 5; i++) {
+  task(i);
+}
+
+function task(i) {
+  setTimeout(function() {
+    document.getElementById("uitkomst").innerHTML = letter[i];
+  }, 2000 * i);
+}
+
